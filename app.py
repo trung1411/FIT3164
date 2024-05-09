dfed08be3bbcd2475c733aa7908fd825967c7f44
John Tran <trung.14112002@gmail.com>
2024-05-10 08:16:41 +1000

    update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import altair as alt
 from vega_datasets import data
 
 
+
 # #Reading the modified_price csv files
 # df = pd.read_csv('modified_price.csv')
 # #Transposing the csv files so that we can perform the data analysis
@@ -61,8 +62,6 @@ data_load_state = st.text('Loading data')
 #Read the data
 dataset = get_modified_price_data()
 #Notify that the data was sucessfully loaded
-data_load_state.text("Done! (using st.cache_data)")
-
 
 st.subheader("Data analysis")
 # st.line_chart(dataset)
@@ -73,14 +72,14 @@ departments = dataset['dept_id'].drop_duplicates()
 
 # Create the sidebar to select products from 
 
-product_choice = st.sidebar.selectbox('Choose the item_id', item)
-departments_choice = st.sidebar.selectbox('Choose the department_id', departments)
+product_choice = st.sidebar.selectbox('Choose the item_id or department_id', item)
+# departments_choice = st.sidebar.selectbox('Choose the department_id', departments)
 
 
 ##################################################################
 #Filtering the dataset based on the input from the users.
 st.header("Individual item price analysis")
-st.markdown("Something something. On the left are select box where users can choose their specific item_id in which they want to choose from. They can also choose their expected iem price change and then below will show a line graph")
+st.markdown("On the left are select box where users can choose their specific item_id or choose to group by department_id in which they wish to observe the change in price elasticity model from. Upon choosing their wanted value, there will be 3 time series graph on the right, with the first one showcasing the change in price of either the individual items or average change in price of the products group by their respective department. The second graph will highlight the change in sales in a similar manner while the final graph will denote the change in elasticity of these products/departments over the span of 253 weeks from week 11101 to 11353 with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
 # Select the rows containing the item_id of the selected item in the select box and manipulate the new rows to create a new data fram suitable to create a line graph to visualise
 new_df = dataset.loc[dataset['item_id'] == product_choice]
 new_df = new_df.transpose().replace(np.nan, 0)
@@ -97,20 +96,14 @@ st.line_chart(new_df)
 
 
 # st.altair_chart(c)
-
-st.markdown("Each of the line chart here display the change in price of each product in percentage in comparison to their previous week, with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
+# st.markdown("Each of the line chart here display the change in price of each product in percentage in comparison to their previous week, with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
 
 # new_df = dataset.loc[dataset['item_id'] == product_choice]
 # new_df2 = new_df.transpose().replace(np.nan,0)
 # a = len(new_df2.columns)
-
+########################################################################
 st.header("Individual item sales analysis")
-st.markdown("In a similar manner, we manipulate the given datasets and aggregrate their sales based on each individual item_id and based on their respective department, and hence produce the graph showcase the percent change in sales of each product over their respective weeks")
-
-
-
-
-
+st.markdown("In a similar manner, we manipulate the given datasets and aggregrate their sales based on each individual item_id and based on their respective department, and hence produce the graph showcase the change in sales of each product over their respective weeks")
 dataset2 = get_modified_sales_data()
 # Create a selection of products without the duplicates
 # item2 = dataset2['item_id'].drop_duplicates()
@@ -118,31 +111,27 @@ dataset2 = get_modified_sales_data()
 # #Create new sidebar
 # product_choice2 = st.sidebar.selectbox('Choose the item_id for sales change', item2)
 # departments_choice2 = st.sidebar.selectbox('Choose the department_id for sales change', departments2)
-
-
 new_df2 = dataset2.loc[dataset2['item_id'] == product_choice]
 new_df3 = new_df2.transpose().replace(np.nan,0)
 # new_df3 = new_df3[0]
 #Dropping the item_id and dept_id row
 new_df3 = new_df3.drop(['item_id', 'dept_id'], axis = "index")
 new_df3 = new_df3.iloc[:, :1]
-new_df3.columns = ['Percent sales change']
+new_df3.columns = ['Sales']
 new_df3['Week'] = [str(x) for x in range(11101, 11354)]
 
 
 # st.line_chart(new_df3)
-c1 = alt.Chart(new_df3).mark_line().encode( x= "Week", y = 'Percent sales change'
-                                          ).properties(title = 'Percent change in sales  over time for product {item_id}'.format(item_id = product_choice))
+c1 = alt.Chart(new_df3).mark_line().encode( x= "Week", y = 'Sales'
+                                          ).properties(title = 'Change in sales  over time for product {item_id}'.format(item_id = product_choice))
 
 
 st.altair_chart(c1)
 
 #Generating a linechart
 # st.line_chart(new_df3)
-
-st.markdown("Each of the line chart here display the change in sales of each product in percentage in comparison to their previous week, with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
-
-
+# st.markdown("Each of the line chart here display the change in sales of each product in percentage in comparison to their previous week, with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
+######################################################
 st.header("Price Elasticity Modelling")
 st.markdown("We originally explore our given Datsets to determine the percent change in price and sales of each products based on their product id, each is stored separately in a dataset. Afterwards, we calulate the price elasticity model of each product by divide the percent change in sales over the percent change in price and aim to observe if each products is either elastic or inelastic. However, upon examining each of the graphs produced, we can conclude that ")
 
@@ -155,18 +144,55 @@ percent_price = get_percent_change_price()
 percent_sales_ind = percent_sales.loc[percent_sales['item_id'] == product_choice]
 percent_sales_ind2 = percent_sales_ind.transpose().replace(np.nan, 0)
 percent_sales_ind2 = percent_sales_ind2.drop(['item_id','dept_id'], axis = "index")
+
+
+
 #Getting the 1st column of the dataset only
 percent_sales_ind2 = percent_sales_ind2.iloc[:, :1]
 percent_sales_ind2.columns = [product_choice]
+percent_sales_ind2['Week'] = [str(x) for x in range(11101, 11354)]
+
 
 percent_price_ind = percent_price.loc[percent_price['item_id'] == product_choice]
 percent_price_ind2 = percent_price_ind.transpose().replace(np.nan, 0)
 percent_price_ind2 = percent_price_ind2.drop(['item_id','dept_id'], axis = "index")
 percent_price_ind2 = percent_price_ind2[:-1]
 percent_price_ind2.columns = [product_choice]
+for i in range(253-157):
+    percent_price_ind2.loc[len(percent_price_ind2)-1] = [0]
 
-st.dataframe(percent_price_ind2)
-st.dataframe(percent_sales_ind2)
+percent_price_ind2['Week'] = [str(x) for x in range(11101, 11354)]
+# percent_sales_ind2[product_choice] /= percent_sales_ind2['Week'].map(percent_price_ind2.set_index('Week')[product_choice]) 
+
+percent_price_ind2.set_index('Week', inplace = True)
+percent_sales_ind2.set_index('Week', inplace = True)
+
+# st.dataframe(percent_price_ind2)
+# st.dataframe(percent_sales_ind2)
+
+price_elasticity_model = []
+#Converting dataframe column to numpy Array
+a = percent_sales_ind2[product_choice].values
+b = percent_price_ind2[product_choice].values
+#As there are 0 values in the dataset, hence we add an extra condition that will return 0 when dividing by zero
+price_elasticity_model = np.divide(a, b, out = np.zeros_like(a), where =b != 0)
+
+price_elasticity_model = pd.DataFrame(price_elasticity_model)
+price_elasticity_model['Week'] = [str(x) for x in range(11101, 11354)]
+price_elasticity_model.set_index('Week')
+price_elasticity_model.columns = ['Elasticity', 'Week']
+
+#Making the time series graph using alt.chart
+c2 = alt.Chart(price_elasticity_model).mark_line().encode( x= "Week", y = 'Elasticity'
+                                          ).properties(title = 'Change in elasticity  over time for product {item_id}'.format(item_id = product_choice))
+
+st.altair_chart(c2)
+
+st.header("Additional Feature Engineering")
+st.markdown("By implementing machine learning using the base RNN model, we are lookikng to predict the potential sales made when user get to choose their expected change in price (discount) and the model would product the expected sales")
+
+number = st.number_input("Please enter the percent discount you want to apply to the product")
+st.write("The current discount applied is {a}%".format(a = number))
 
 # price_elasticity_model_ind = percent_price_ind2[]
 
