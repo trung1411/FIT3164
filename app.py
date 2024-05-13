c865217c9a11c0d21cd850303eedbee28e3bca8e
John Tran <trung.14112002@gmail.com>
2024-05-13 22:33:32 +1000

    update app.py

--- app.py
+++ app.py
@@ -9,6 +9,35 @@ import time
 import altair as alt
 from vega_datasets import data
 import h5py 
+import streamlit_authenticator as stauth
+
+
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    if username == 'jsmith':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 1')
+    elif username == 'rbriggs':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 2')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
 # import tensorflow as tf
 # from tensorflow.python.keras.models import Model
