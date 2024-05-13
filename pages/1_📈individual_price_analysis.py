b983388358e3b7c3852b9e01d7d3d3f7dee73c0e
John Tran <trung.14112002@gmail.com>
2024-05-14 02:13:33 +1000

    update page 1

--- pages/1_📈individual_price_analysis.py
+++ pages/1_📈individual_price_analysis.py
@@ -32,7 +32,7 @@ item = dataset['item_id'].drop_duplicates()
 item = item[0: len(item.index) -7, ]
 
 # Create the sidebar to select products from 
-product_choice = st.sidebar.selectbox('Choose the item_id or department_id', item)
+product_choice = st.sidebar.selectbox('Choose the item_id', item)
 
 #############################
 st.markdown("On the left are select box where users can choose their specific item_id or choose to group by department_id in which they wish to observe the change in price elasticity model from. Upon choosing their wanted value, there will be 3 time series graph on the right, with the first one showcasing the change in price of either the individual items or average change in price of the products group by their respective department. The second graph will highlight the change in sales in a similar manner while the final graph will denote the change in elasticity of these products/departments over the span of 253 weeks from week 11101 to 11353 with the first week being labeled 11101 which starts counting from Saturday 29/01/2011")
