5282c77a5215e3543c7b6f9c2a163a7351f82e40
John Tran <trung.14112002@gmail.com>
2024-05-17 18:42:11 +1000

    update page 7

--- pages/7_expected_sales_volume.py
+++ pages/7_expected_sales_volume.py
@@ -77,49 +77,23 @@ st.write("The %change in sales volume will be 4.5%")
 file = st.file_uploader("Please choose a file")
 
 if file is not None:
-
     #To read file as bytes:
-
     bytes_data = file.getvalue()
-
     st.write(bytes_data)
-
-
-
     #To convert to a string based IO:
-
     stringio = StringIO(file.getvalue().decode("utf-8"))
-
     st.write(stringio)
-
-
-
     #To read file as string:
-
     string_data = stringio.read()
-
     st.write(string_data)
-
-
-
     #Can be used wherever a "file-like" object is accepted:
-
     df= pd.read_csv(file)
-
     st.write(df)
-
-
-
 #adding a file uploader to accept multiple CSV files
-
 uploaded_files = st.file_uploader("Please choose a CSV file you want us to analyse the price elasticity, filee format must be the same as the format shown above", accept_multiple_files=True)
-
 for file in uploaded_files:
-
     bytes_data = file.read()
-
     st.write("File uploaded:", file.name)
-
     st.write(bytes_data)
 
 
