e781b167608139508cb72f6f8d107090f248ab87
John Tran <trung.14112002@gmail.com>
2024-05-17 19:25:19 +1000

    update test

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,10 +13,19 @@ from streamlit.testing.v1 import AppTest
 def test_logout():
     "A user tries to logout from the project intro page"
     at = AppTest.from_file("../pages/0_project_introduction_page.py").run()
-    at.button[0].click().run()
+    at.button[1].click().run()
     assert at.switch_page
 
-    at.
+def test_traversing_through_pages():
+    "Assuming users start at the project introduction page"
+    at = AppTest.from_file("../pages/0_project_introduction_page.py")
+    at.button[0].click().run()
+    # Assert if there is a page switch
+    assert at.switch_page
+
+    #Assume the users pick one value of item_id from the selectbox in the sidebar
+    at.sidebar.selectbox[0].select("HOUSEHOLD_2_516").run()
+    assert at.
 
 # def test_individual_price_analysis_page():
 #     "A user tries to select the item_id from the page"
