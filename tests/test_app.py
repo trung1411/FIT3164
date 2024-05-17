f2f747fc21a3e009de9bc31810dae5255f6416de
John Tran <trung.14112002@gmail.com>
2024-05-17 18:37:42 +1000

    update test app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,13 @@
 """test_app.py"""
 from streamlit.testing.v1 import AppTest
 
-def test_login():
-    "A user input the username and password, then clicks the login button"
-    at = AppTest.from_file("app.py").run()
-    at.text_input[0].input("jsmith").run()
-    at.text_input[1].input("abcd").run()
-    at.button[0].click().run()
-    assert at.error
+# def test_login():
+#     "A user input the username and password, then clicks the login button"
+#     at = AppTest.from_file("app.py").run()
+#     at.text_input[0].input("jsmith").run()
+#     at.text_input[1].input("abcd").run()
+#     at.button[0].click().run()
+#     assert at.error
 
 
 def test_logout():
@@ -16,6 +16,6 @@ def test_logout():
     at.button[0].click().run()
     assert at.switch_page
 
-def test_individual_price_analysis_page():
-    "A user tries to select the item_id from the page"
-    at = AppTest.from_file("../pages/1_individual_price_analysis")
+# def test_individual_price_analysis_page():
+#     "A user tries to select the item_id from the page"
+#     at = AppTest.from_file("../pages/1_individual_price_analysis")
