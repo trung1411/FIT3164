762039ebd9d61b39cba4255f00e1b9c2586bf886
John Tran <trung.14112002@gmail.com>
2024-05-17 19:11:00 +1000

    update page 3

--- pages/3_📈department_sales_analysis.py
+++ pages/3_📈department_sales_analysis.py
@@ -45,4 +45,4 @@ if st.button("Next page"):
     st.switch_page("pages/4_💲price_elasticity_modelling.py")
 
 if st.button("Go back"):
-    st.switch_page("pages/2_📈indidividual_sales_analysis.py")
+    st.switch_page("pages/2_📈individual_sales_analysis.py")
