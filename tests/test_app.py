7e668b5ae23384ab30f3ec045b698c581bb79947
John Tran <trung.14112002@gmail.com>
2024-05-17 17:53:22 +1000

    update test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,6 @@
 """test_app.py"""
 from streamlit.testing.v1 import AppTest
 
-
-
-
 def test_login():
     "A user input the username and password, then clicks the login button"
     at = AppTest.from_file("app.py").run()
@@ -13,3 +10,12 @@ def test_login():
     assert at.error[0].value == "Username/password is incorrect"
 
 
+def test_logout():
+    "A user tries to logout from the project intro page"
+    at = AppTest.from_file("../pages/0_project_introduction_page.py").run()
+    at.button[0].click().run()
+    assert at.switch_page
+
+def test_individual_price_analysis_page():
+    "A user tries to select the item_id from the page"
+    at = AppTest.from_file("../pages/1_individual_price_analysis")
